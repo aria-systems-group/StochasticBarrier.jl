8916870334a6c1e4e49e4a77a16317676d423f3b
Rayan Mazouz <rayan.mazouz@colorado.edu>
2026-02-16 14:16:46 -0700

    Cut some of the benchmarks

--- StochasticBarrierFunctions/run_sos.bash
+++ StochasticBarrierFunctions/run_sos.bash
@@ -31,7 +31,6 @@ yaml_files = [
     "benchmarks/linear/systems/room/SOS/sos_deg4.yaml",
     "benchmarks/linear/systems/room/SOS/sos_deg6.yaml",
     "benchmarks/linear/systems/room/SOS/sos_deg8.yaml",
-    "benchmarks/linear/systems/room/SOS/sos_deg12.yaml",
 
     "benchmarks/linear/systems/quadrotor/SOS/sos_deg2.yaml",
     "benchmarks/linear/systems/quadrotor/SOS/sos_deg4.yaml",
@@ -44,8 +43,6 @@ yaml_files = [
     "benchmarks/polynomial/systems/thermostat/SOS/sos_deg8.yaml",
     "benchmarks/polynomial/systems/thermostat/SOS/sos_deg12.yaml",
 
-    "benchmarks/polynomial/systems/oscillator/SOS/sos_deg2.yaml",
-    "benchmarks/polynomial/systems/oscillator/SOS/sos_deg4.yaml",
     "benchmarks/polynomial/systems/oscillator/SOS/sos_deg6.yaml",
     "benchmarks/polynomial/systems/oscillator/SOS/sos_deg8.yaml",
     "benchmarks/polynomial/systems/oscillator/SOS/sos_deg12.yaml"
